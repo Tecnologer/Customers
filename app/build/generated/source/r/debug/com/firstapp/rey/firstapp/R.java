449285650a17e758953aef65aa8035c9d23f33e7
rdominguez <rdominguez@tecnologer.net>
2016-08-04 17:10:07 +0300

    First Commit - Change main icon

--- app/build/generated/source/r/debug/com/firstapp/rey/firstapp/R.java
+++ app/build/generated/source/r/debug/com/firstapp/rey/firstapp/R.java
@@ -1781,6 +1781,7 @@ containing a value of this type.
         public static final int ic_add_black_24dp=0x7f030000;
         public static final int ic_add_white_24dp=0x7f030001;
         public static final int ic_launcher=0x7f030002;
+        public static final int ic_taco_green_24dp=0x7f030003;
     }
     public static final class string {
         public static final int BtnShowCustomers=0x7f060014;
